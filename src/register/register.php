d4a5eb72e82ef4ae32f21a18d4a9e00c7d31604a
raeszogaj <raeszogaj@gmail.com>
2025-05-17 01:19:48 +0200

    Register

---
+++ src/register/register.php
@@ -0,0 +1,67 @@
+<?php 
+
+include_once('../../db_connection/index.php');
+
+$errors = array();
+
+if (isset($_POST['register_btn'])) {
+    $name = $_POST['name'];
+    $password = $_POST['password'];
+    $confirmPassword = $_POST['confirmPassword'];
+    $email = $_POST['email'];
+
+    if (empty($name)) {
+        array_push($errors, "name is required");
+    } elseif(!preg_match("/^[a-zA-ZëË ]*$/", $name)) {
+        array_push($errors, "Only letters and white space allowed");
+    }
+
+    if (empty($email)) {
+        array_push($errors, "Email is required");
+    } elseif(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        array_push($errors, "Invalid email format");
+    }
+
+    if (empty($password)) {
+        array_push($errors, "Password is required");
+    }
+    if($password != $confirmPassword) {
+        array_push($errors, "Password nuk eshte i njejt");
+    }
+
+    $query = "SELECT * FROM users WHERE name=:name OR email=:email";
+    $checkQuery = $conn->prepare($query);
+    $checkQuery->bindParam(":name", $name);
+    $checkQuery->bindParam(":email", $email);
+    $checkQuery->execute();
+    $result = $checkQuery->fetchAll();
+
+    foreach ($result as $row) {
+        if ($row['name'] == $name) {
+            array_push($errors, "name already exists");
+        }
+        if ($row['email'] == $email) {
+            array_push($errors, "Email already exists");
+        }
+    }
+
+    if (count($errors) == 0) {
+
+        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+        //delete $hashedPassword when we want to see the password and change  $sqlQuery->bindParam(":password", $hashedPassword); to   $sqlQuery->bindParam(":password", $password);
+
+        $sql = "INSERT INTO users (name, password, email) VALUES (:name, :password, :email)";
+        $sqlQuery = $conn->prepare($sql);
+
+        $sqlQuery->bindParam(":name", $name);
+        $sqlQuery->bindParam(":password", $hashedPassword);
+        $sqlQuery->bindParam(":email", $email);
+
+        $sqlQuery->execute();
+
+        echo "<script>alert('Register succesfully!')</script>";
+    }
+}
+
+?>
